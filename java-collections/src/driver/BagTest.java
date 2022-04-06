834a4547e50e80e3c55cf827ee3d1d84c6ed78fe
Josh Archer <jarcher@greenriver.edu>
2022-04-06 10:32:42 -0700

    Added size() and isEmpty().

--- java-collections/src/driver/BagTest.java
+++ java-collections/src/driver/BagTest.java
@@ -7,15 +7,19 @@ public class BagTest
     public static void main(String[] args)
     {
         Bag bagOfColors = new Bag(5);
+        System.out.println("Empty? " + bagOfColors.isEmpty());
 
         bagOfColors.add("red");
         bagOfColors.add("green");
         bagOfColors.add("yellow");
-        bagOfColors.add("blue");
+        /*bagOfColors.add("blue");
         bagOfColors.add("purple");
-        bagOfColors.add("beige");
+        bagOfColors.add("beige");*/
 
         System.out.println(bagOfColors);
+        System.out.println("Size: " + bagOfColors.size());
+        System.out.println("Capacity: " + bagOfColors.capacity());
+        System.out.println("Empty? " + bagOfColors.isEmpty());
 
         try
         {
@@ -26,5 +30,16 @@ public class BagTest
         {
             System.out.println("Bag is working");
         }
+
+        //create a bag of 1,000,000 numbers
+        Bag bagOfNumbers = new Bag(1000000);
+
+        System.out.println("Started adding numbers");
+        for (int i = 1; i <= 1000000; i++)
+        {
+            bagOfNumbers.add(i);
+        }
+        System.out.println("Ended adding numbers");
+        System.out.println("Size: " + bagOfNumbers.size());
     }
 }
--- java-collections/src/structures/Bag.java
+++ java-collections/src/structures/Bag.java
@@ -6,12 +6,12 @@ public class Bag implements ICollection
 {
     //store our elements in an array
     private Object[] data;
+    private int nextIndex = 0;
 
     public Bag(int capacity)
     {
         //precondition(s)
-        if (capacity < 0)
-        {
+        if (capacity < 0) {
             throw new IllegalStateException("Capacity must be non-negative");
         }
 
@@ -21,7 +21,16 @@ public class Bag implements ICollection
     @Override
     public boolean add(Object element)
     {
-        for (int i = 0; i < data.length; i++)
+        //is the bag full?
+        if (nextIndex == data.length) {
+            return false;
+        }
+
+        data[nextIndex] = element;
+        nextIndex++;
+        return true;
+
+        /*for (int i = 0; i < data.length; i++)
         {
             //is there a spot that is open?
             if (data[i] == null)
@@ -30,7 +39,26 @@ public class Bag implements ICollection
                 return true;
             }
         }
-        return false;
+        return false;*/
+    }
+
+    @Override
+    public int size()
+    {
+        return nextIndex;
+    }
+
+    public int capacity()
+    {
+        return data.length;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        //return nextIndex == 0;
+        //return size() == 0;
+        return data[0] == null;
     }
 
     @Override
@@ -41,18 +69,6 @@ public class Bag implements ICollection
 
     @Override
     public boolean remove(Object element)
-    {
-        return false;
-    }
-
-    @Override
-    public int size()
-    {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty()
     {
         return false;
     }
