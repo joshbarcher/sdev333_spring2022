e928115ba5b0c1204a7184717738d2a871acd598
Josh Archer <jarcher@greenriver.edu>
2022-06-06 10:41:27 -0700

    Added an add() method to the hash table.

--- hashtables/src/hashing/HashTable.java
+++ hashtables/src/hashing/HashTable.java
@@ -4,27 +4,58 @@ import java.util.Iterator;
 
 public class HashTable<T> implements IHashTable<T>
 {
-    private Element[] table;
+    public static final int INITIAL_TABLE_SIZE = 10;
+    public static final int RESIZE_RATE = 2;
+    public static final double MAX_LOAD_FACTOR = 0.5;
+
+    private ElementWrapper[] table;
+    private int size;
 
     public HashTable()
     {
-        table = new Element[10];
-    }
-
-    @Override
-    public void add(T... elements)
-    {
-
+        table = new ElementWrapper[INITIAL_TABLE_SIZE];
     }
 
     @Override
     public boolean add(T element)
     {
+        //is the table too full? (have we exceeded the load factor)
+        double curLoadFactor = (double)size / table.length;
+        if (curLoadFactor > MAX_LOAD_FACTOR)
+        {
+            resize();
+        }
+
+        //no duplicates in a hash table (this is a set structure!)
+        if (contains(element))
+        {
+            return false; //exit
+        }
+
+        //get a positive hash code and index
+        int code = Math.abs(element.hashCode());
+        int index = code % table.length;
+
+        //insert the element
+        //use probing (linear) to search for a spot in the table
+        while (table[index] != null)
+        {
+            index = (index + 1) % table.length;
+        }
+
+        table[index] = new ElementWrapper(element);
+        size++;
 
         return true;
     }
 
     private void resize()
+    {
+        //TODO write this later
+    }
+
+    @Override
+    public void add(T... elements)
     {
 
     }
@@ -71,12 +102,18 @@ public class HashTable<T> implements IHashTable<T>
         return null;
     }
 
-    private static class Element<T>
+    private static class ElementWrapper<T>
     {
         private T element;
         private boolean removed;
 
-        public Element(T element, boolean removed)
+        public ElementWrapper(T element)
+        {
+            this.element = element;
+            removed = false;
+        }
+
+        public ElementWrapper(T element, boolean removed)
         {
             this.element = element;
             this.removed = removed;
--- hashtables/src/test/TestHashTable.java
+++ hashtables/src/test/TestHashTable.java
@@ -1,9 +1,21 @@
 package test;
 
+import hashing.HashTable;
+
 public class TestHashTable
 {
     public static void main(String[] args)
     {
+        HashTable<String> wordTable = new HashTable<>();
 
+        wordTable.add("apple");
+        wordTable.add("grape");
+        wordTable.add("banana");
+        wordTable.add("cantalope");
+        wordTable.add("raisin");
+        wordTable.add("kiwi");
+        wordTable.add("mango");
+
+        System.out.println(wordTable);
     }
 }
